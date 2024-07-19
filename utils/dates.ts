f5c9369319d0d290f84a0f973960d79fa60b637b
alanagoyal <alanaan@gmail.com>
2024-07-19 12:36:22 -0700

    fix dates

--- utils/dates.ts
+++ utils/dates.ts
@@ -1,24 +1,27 @@
-export function formatEventDates(startTimestampz: string, endTimestampz: string) {
-    const startDate = new Date(startTimestampz);
-    const endDate = new Date(endTimestampz);
-  
-    const month = startDate.toLocaleString("en-US", { month: "short" }).toUpperCase();
-    const day = startDate.getDate();
-    const formattedDate = startDate.toLocaleDateString("en-US", {
-      weekday: "long",
-      month: "long",
-      day: "numeric",
-      year: "numeric",
-    });
-    const formattedTime = `${startDate.toLocaleTimeString("en-US", {
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true,
-    })} - ${endDate.toLocaleTimeString("en-US", {
-      hour: "numeric",
-      minute: "numeric",
-      hour12: true,
-    })}`;
-  
-    return { month, day, formattedDate, formattedTime };
-  }
+export function formatEventDates(start: string, end: string) {
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+
+  const month = startDate.toLocaleString('default', { month: 'short' });
+  const day = startDate.getDate();
+
+  const formattedDate = startDate.toLocaleDateString('en-US', {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    timeZone: 'America/Los_Angeles'
+  });
+
+  const formattedTime = `${startDate.toLocaleTimeString('en-US', { 
+    hour: 'numeric', 
+    minute: '2-digit',
+    timeZone: 'America/Los_Angeles'
+  })} - ${endDate.toLocaleTimeString('en-US', { 
+    hour: 'numeric', 
+    minute: '2-digit',
+    timeZone: 'America/Los_Angeles'
+  })}`;
+
+  return { month, day, formattedDate, formattedTime };
+}
