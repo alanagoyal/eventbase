7f03b516e17269bc9b94025e597a3ccf68aa29aa
alanagoyal <alanaan@gmail.com>
2024-07-19 18:00:22 -0700

    fix date

--- utils/dates.ts
+++ utils/dates.ts
@@ -1,26 +1,28 @@
 export function formatEventDates(start: string, end: string) {
+  const options = { timeZone: 'America/Los_Angeles' };
   const startDate = new Date(start);
   const endDate = new Date(end);
 
-  const month = startDate.toLocaleString('default', { month: 'short' });
-  const day = startDate.getDate();
+  const pacificStartDate = new Date(startDate.toLocaleString('en-US', options));
+  
+  const month = pacificStartDate.toLocaleString('default', { month: 'short' });
+  const day = pacificStartDate.getDate();
 
-  const formattedDate = startDate.toLocaleDateString('en-US', {
+  const formattedDate = pacificStartDate.toLocaleDateString('en-US', {
     weekday: 'long',
     year: 'numeric',
     month: 'long',
     day: 'numeric',
-    timeZone: 'America/Los_Angeles'
   });
 
   const formattedTime = `${startDate.toLocaleTimeString('en-US', { 
     hour: 'numeric', 
     minute: '2-digit',
-    timeZone: 'America/Los_Angeles'
+    ...options
   })} - ${endDate.toLocaleTimeString('en-US', { 
     hour: 'numeric', 
     minute: '2-digit',
-    timeZone: 'America/Los_Angeles'
+    ...options
   })}`;
 
   return { month, day, formattedDate, formattedTime };
