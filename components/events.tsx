29d67695ab69f8a1ddbe760baf910d5bc5cbcefa
alanagoyal <alanaan@gmail.com>
2024-07-21 11:18:49 -0700

    new event button on events page

--- components/events.tsx
+++ components/events.tsx
@@ -2,6 +2,8 @@
 
 import { EventWithHostAndType } from "@/types/events";
 import EventCard from "./event-card";
+import { Button } from "./ui/button";
+import { Plus } from "lucide-react";
 
 export default function Events({
   allRsvps,
@@ -12,7 +14,10 @@ export default function Events({
 }) {
   return (
     <div className="flex flex-col items-start mih-h-dvh p-12 w-full md:w-4/5">
-      <h1 className="text-2xl font-bold py-4">Events</h1>
+      <div className="flex justify-between items-center w-full py-4">
+        <h1 className="text-2xl font-bold">Events</h1>
+        <Button variant="ghost"><Plus className="w-4 h-4 mr-2" /> New Event</Button>
+      </div>
       <div className="flex flex-col gap-4 w-full">
         {allRsvps.map((event) => (
           <EventCard key={event.id} event={event} />
