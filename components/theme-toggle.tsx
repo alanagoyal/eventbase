eb2ed27266753970c68a565471fd5da249c549ea
alanagoyal <alanaan@gmail.com>
2024-07-26 15:25:17 -0700

    fix hydration error

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -7,6 +7,15 @@ import { Moon, Sun } from "lucide-react";
 
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
 
   return (
     <Button
