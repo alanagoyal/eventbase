4ec105e87dd25cbbe8c151d5eace25631999edd1
alanagoyal <alanaan@gmail.com>
2024-07-25 19:05:07 -0700

    try to fix apple touch icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,21 +43,6 @@ export default async function RootLayout({
 
   return (
     <html lang="en" suppressHydrationWarning>
-      <head>
-        <title>{siteConfig.name}</title>
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
-        <meta property="twitter:card" content="summary_large_image"></meta>
-        <meta property="twitter:title" content={siteConfig.name}></meta>
-        <meta
-          property="twitter:description"
-          content={siteConfig.description}
-        ></meta>
-        <meta property="og:site_name" content={siteConfig.name}></meta>
-        <meta property="og:description" content={siteConfig.description}></meta>
-        <meta property="og:title" content={siteConfig.name}></meta>
-        <meta property="og:url" content={siteConfig.url}></meta>
-      </head>
       <body
         className={cn(
           "mih-h-dvh bg-background font-sans antialiased",
